74b2ee9169f310b833e8098eeb6a185a9862526a
Nico Elvino <nicoelvino@gmail.com>
2024-09-19 10:57:18 +1000

    dataloader added to the proyect, to batch db calls and improve performance

--- db/companies.js
+++ db/companies.js
@@ -1,7 +1,17 @@
+import DataLoader from 'dataloader';
 import { connection } from './connection.js';
 
 const getCompanyTable = () => connection.table('company');
 
 export async function getCompany(id) {
   return await getCompanyTable().first().where({ id });
+}
+// in order to make less calls to the database, we use the dataloader to preload the data before using it, but we add it to the context in server.js 
+// so its always called again when needed, to avoid cashing issues. 
+export function createCompanyLoader() {
+  return new DataLoader(async (ids) => {
+    console.log('[companyLoader] ids:', ids);
+    const companies = await getCompanyTable().select().whereIn('id', ids);
+    return ids.map((id) => companies.find((company) => company.id === id));
+  });
 }
--- resolvers.js
+++ resolvers.js
@@ -1,19 +1,20 @@
-import { getCompany } from "./db/companies.js";
-import { createJob, getJob, getJobs, getJobsByCompany, updateJob } from "./db/jobs.js";
+import { GraphQLError } from 'graphql';
+import { getCompany } from './db/companies.js';
+import { createJob, deleteJob, getJob, getJobs, getJobsByCompany, updateJob } from './db/jobs.js';
 
 export const resolvers = {
   Query: {
     company: async (_root, { id }) => {
       const company = await getCompany(id);
       if (!company) {
-        throw notFoundError("No Company found with id " + id);
+        throw notFoundError('No Company found with id ' + id);
       }
       return company;
     },
     job: async (_root, { id }) => {
       const job = await getJob(id);
       if (!job) {
-        throw notFoundError("No Job found with id " + id);
+        throw notFoundError('No Job found with id ' + id);
       }
       return job;
     },
@@ -56,14 +57,16 @@ export const resolvers = {
   },
 
   Job: {
-    company: (job) => getCompany(job.companyId),
+    company: (job, _args, { companyLoader }) => {
+      return companyLoader.load(job.companyId);
+    },
     date: (job) => toIsoDate(job.createdAt),
   },
 };
 
 function notFoundError(message) {
   return new GraphQLError(message, {
-    extensions: { code: "NOT_FOUND" },
+    extensions: { code: 'NOT_FOUND' },
   });
 }
 
@@ -74,5 +77,5 @@ function unauthorizedError(message) {
 }
 
 function toIsoDate(value) {
-  return value.slice(0, "yyyy-mm-dd".length);
+  return value.slice(0, 'yyyy-mm-dd'.length);
 }
